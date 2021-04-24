c6d4800a010f2daa1f8a828f900cd710db7c97ea
OlgaPetrova3103 <73943461+OlgaPetrova3103@users.noreply.github.com>
2021-04-24 21:17:27 +0300

    Update Program.cs

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,92 +1,47 @@
+
 using System;
 using System.Text;
 using System.IO;
 
-/*
- * namespace generator
-{
-    class CharGenerator 
-    {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
-        private Random random = new Random();
-        public CharGenerator() 
-        {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
-        }
-        public char getSym() 
-        {
-           return data[random.Next(0, size)]; 
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
-            {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
-            }
-            Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
-            {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
-            }
-            
-        }
-    }
-}
- */
+
 
 namespace generator
 {
     class Zadanie_1
     {
         private string syms = "абвгдежзийклмнопрстуфхцчшщьыэюя";
-        private char[] data;
-        private int size;
         private Random random = new Random();
-        public int[,] arr; 
+        private int[,] arr; 
         int summa = 0;
         public Zadanie_1(string file)
         {
             arr = new int[31, 31];
             using (StreamReader r = new StreamReader(file))
             {
-                for (int i = 0; i < 31; i++)
+                for (int i = 0; i < 30; i++)
                 {
                     string test = r.ReadLine();
                     string[] temp = test.Split(new Char[] { ' ' });
-                    for (int j = 0; j < 31; j++)
+                    for (int j = 0; j < 30; j++)
                     {
-                        arr[i, j] = int.Parse(temp[j]);
+                        arr[i, j] = Int32.Parse(temp[j]);
                     }
                 }
             }
-            size = syms.Length;//число букв алфавита
-            data = syms.ToCharArray();
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
+            for (int i = 0; i < 30; i++)
+                for (int j = 0; j < 30; j++)
                     summa += arr[i, j];
         }
-        public string getSym()
+        public string get_str()
         {
             int m = random.Next(0, summa);
             int j = 0;
             int i = 0;
 
-            for (i = 0; i < size; i++)
+            for (i = 0; i < 30; i++)
             {
                 int F = 0;
-                for (j = 0; j < size; j++)
+                for (j = 0; j < 30; j++)
                 {
                     if (m < arr[i, j])
                     {
@@ -96,7 +51,7 @@ namespace generator
                 }
                 if (F == 1) break;
             }
-            return data[i].ToString() + data[j].ToString() ;
+            return syms[i].ToString() + syms[j].ToString();
         }
     }
     class Zadanie_2
@@ -113,7 +68,7 @@ namespace generator
             words = new string[100];
             using (StreamReader sr = new StreamReader(file))
             {
-                for (int i = 0; i < 100; ++i)
+                for (int i = 0; i < 100; i++)
                 {
                     string line = sr.ReadLine();
                     string[] temp = line.Split(new Char[] { ' ' });
@@ -123,7 +78,7 @@ namespace generator
                 }
             }
         }
-            public string getSym()
+            public string get_str()
         {
             int m = random.Next(0, summa);
             int i = 0;
@@ -138,36 +93,33 @@ namespace generator
         private Random random = new Random();
         int summa = 0;
         public int[] ver;
-        public string[] words1;
-        public string[] words2;
+        public string[] words;
         public int sum;
         public Zadanie_3(string file)
         {
             sum = 0;
             ver = new int[100];
-            words1 = new string[100];
-            words2 = new string[100];
+            words = new string[100];
             using (StreamReader sr = new StreamReader(file))
             {
-                for (int i = 0; i < 100; ++i)
+                for (int i = 0; i < 100; i++)
                 {
                     string line = sr.ReadLine();
                     string[] temp = line.Split(new Char[] { ' ' });
-                    words1[i] = temp[0];
-                    words2[i] = temp[1];
-                    ver[i] = Int32.Parse(temp[3]);
-                    sum += ver[i];
+                    words[i] = temp[0];
+                    summa += i;
+                    ver[i] = summa;
                 }
             }
         }
-        public string getSym()
+        public string get_str()
         {
             int m = random.Next(0, summa);
             int i = 0;
             for (i = 0; i < 100; i++)
-                if (m < ver[i])
-                    break;
-            return words1[i].ToString() + words2[i].ToString();
+                    if (m < ver[i])
+                        break;
+            return words[i];
         }
     }
     class Program
@@ -175,14 +127,14 @@ namespace generator
         static void Main(string[] args)
         {
             Zadanie_1 z1 = new Zadanie_1("task_1.txt");
-            Zadanie_1 z2 = new Zadanie_1("task_2.txt");
-            Zadanie_1 z3 = new Zadanie_1("task_3.txt");
+            Zadanie_2 z2 = new Zadanie_2("task_2.txt");
+            Zadanie_3 z3 = new Zadanie_3("task_3.txt");
 
             using (StreamWriter r = new StreamWriter("otv_1.txt"))
             {
                 for (int i = 0; i < 1000; i++)
                 {
-                    string str = z1.getSym();
+                    string str = z1.get_str();
                     r.Write(str[0]);
                     r.Write(str[1]);
                     r.Write(" ");
@@ -192,7 +144,7 @@ namespace generator
             {
                 for (int i = 0; i < 1000; i++)
                 {
-                    string str = z2.getSym();
+                    string str = z2.get_str();
                     for (int j = 0; j < str.Length; j++)
                         r.Write(str[j]);
                         r.Write(' ');
@@ -202,7 +154,7 @@ namespace generator
             {
                 for (int i = 0; i < 1000; i++)
                 {
-                    string str = z3.getSym();
+                    string str = z3.get_str();
                     for (int j = 0; j < str.Length; j++)
                         r.Write(str[j]);
                         r.Write(' ');
@@ -212,4 +164,3 @@ namespace generator
         }
     }
 }
-
